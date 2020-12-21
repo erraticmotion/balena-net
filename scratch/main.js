8138806d5f1bece1a39a67a5aa751fdc3a4f16be
Rob Ives <robert.ives@orange.fr>
2020-12-21 14:14:06 +0000

    Add docker compose

--- scratch/main.js
+++ scratch/main.js
@@ -3,9 +3,9 @@
 var express = require('express');
 var app = express();
 
-// reply to request with "Hello World!"
+// reply to request with "Hello World"
 app.get('/', function (req, res) {
-  res.send("Hello World on VM Nuc running balena OS");
+  res.send("Hello World release 7.");
 });
 
 //start a server on port 80 and log its start to our console
